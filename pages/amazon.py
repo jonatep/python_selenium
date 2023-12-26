b36e336ebc3534f103be166f515d5a60ff22249c
jonatep <jonatep8@gmail.com>
2023-12-26 13:22:52 +0100

    fix: mejor seleccion de primera imagen

--- pages/amazon.py
+++ pages/amazon.py
@@ -10,15 +10,21 @@ wait = WebDriverWait(driver, timeout=2.5)
 
 variations_to_product = "//div[@id='inline-twister-expander-content-color_name']//li[not(contains(@class, 'swatch-prototype'))]//img"
 main_image_product = "(//div[contains(@class, 'imgTagWrapper')]/img)[1]"
+first_image = "//div[contains(@class, 'imgTagWrapper')]//img"
 
 def browse_to_amazon():
     driver.get('https://www.amazon.com')
-    
+
 def go_to_product(product):
     driver.get(product)
 
 def click_second_and_first_product():
-    first_product = driver.find_element(By.XPATH, variations_to_product.replace('//img', '[1]'))
+    try:
+        first_product = driver.find_element(By.XPATH, variations_to_product.replace('//img', '[1]'))
+    except:
+        print('Website version not supported')
+        driver.close()
+        
     second_product = driver.find_element(By.XPATH, variations_to_product.replace('//img', '[2]'))
     second_product.click()
     first_product.click()
@@ -29,7 +35,7 @@ def is_image_changing_when_hovering():
     actions = ActionChains(driver)
     res = []
     wait.until(lambda d : driver.find_element(By.XPATH, main_image_product)).is_displayed()
-    previous_image = driver.find_element(By.XPATH, main_image_product).get_attribute('src')
+    previous_image = driver.find_element(By.XPATH, first_image).get_attribute('src')
     
     for element in elements:
         common.perform_hover_by_element(element, actions)
