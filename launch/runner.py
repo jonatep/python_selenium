6c88ca905df736f864e94c6c0dc6ec9dc3c37199
jonatep <jonatep8@gmail.com>
2023-12-26 08:55:35 +0100

    add: eliminacion de archivos del directorio de reportes

--- launch/runner.py
+++ launch/runner.py
@@ -1,5 +1,15 @@
 import sys
+import os
 from behave.__main__ import main as behave_main
+
+absolute_path = os.path.dirname(__file__)
+results_folder = "allure-results"
+full_path = absolute_path.replace('launch', results_folder)
+
+for root, dirs, files in os.walk(full_path):
+    for file in files:
+        if file.endswith('.json'):
+            os.remove(os.path.join(root, file))
 
 if __name__ == "__main__":
     sys.exit(behave_main('--tags=\"@test\"'))
