4f81a013c5dcf90cc777aa5ec6931a1ed622a9a8
jonatep <jonatep8@gmail.com>
2023-12-27 11:25:39 +0100

    add: añadida comprobacion de divisas

--- features/steps/amazon_steps.py
+++ features/steps/amazon_steps.py
@@ -5,10 +5,19 @@ from pages import amazon as amazon_driver
 def step_impl(context):
     amazon_driver.browse_to_amazon()
     
-@when('I go to a {product}')
+@given('I go to an Amazon {product}')
 def step_impl(context, product):
     amazon_driver.go_to_product(product)
-    
+
+@when('I go to the previous tab')
+def step_impl(context):
+    amazon_driver.go_to_previous_tab()
+
+@when('I select the currency {currency} in the currency dropdown')
+def step_impl(context, currency):
+    amazon_driver.go_to_currency_change_page()
+    amazon_driver.select_currency_from_dropdown(currency)
+
 @then('I can assert that, when hovering over variations of the product, the image changes')
 def step_impl(context):
     assert amazon_driver.is_image_changing_when_hovering() == True
@@ -21,10 +30,6 @@ def step_impl(context):
 def step_impl(context):
     amazon_driver.click_on_coupon_checkbox()
     assert "https://www.amazon.com/ap/signin?" in amazon_driver.get_current_url()
-    
-@when('I go to the previous tab')
-def step_impl(context):
-    amazon_driver.go_to_previous_tab()
 
 # This test will result in a failure, not because of the design of the test itself,
 # but because Amazon doesn't take into account this case when selecting checkboxes
@@ -32,5 +37,9 @@ def step_impl(context):
 def step_impl(context):
     assert amazon_driver.is_checkbox_selected() == False
     
+@then('The products are displayed with the currency {currency}')
+def step_impl(context, currency):
+    shown_currency = amazon_driver.get_current_currency()
+    assert shown_currency == currency
 
-
+    
--- pages/amazon.py
+++ pages/amazon.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from . import common
 import re
 import math 
@@ -19,6 +20,11 @@ original_price = "//div[contains(@id, 'corePriceDisplay')]//span[contains(@class
 discount = "//div[contains(@id, 'corePriceDisplay')]//span[contains(@class, 'savingPriceOverride ')]"
 checkbox_coupon = "//label[contains(@for, 'checkbox')]//i[contains(@class, 'checkbox')]"
 checkbox_input = "//label[contains(@for, 'checkbox')]//input[contains(@id, 'checkbox')]"
+language_nav_tool = "//div[@id='nav-tools']//a[contains(@id, 'icp')]"
+change_currency_link = "//div[@id = 'nav-flyout-icp']//a[contains(@class, 'change')]"
+currency_dropdown = "//p[contains(@id, 'currency-dropdown')]//span[contains(@id, 'dropdown')]"
+specific_currency = "//div[contains(@class, 'wrapper')]//li[@role='option' and @id='CURRENCY']"
+confirm_currency_button = "(//span[contains(@id, 'save-button')])[1]//input"
 
 def browse_to_amazon():
     driver.get('https://www.amazon.com')
@@ -75,3 +81,31 @@ def go_to_previous_tab():
 def is_checkbox_selected():
     checkbox = driver.find_element(By.XPATH, checkbox_input)
     return checkbox.is_selected()
+
+def go_to_currency_change_page():
+    actions = ActionChains(driver)
+    common.perform_hover_by_xpath(language_nav_tool, actions, driver)
+    wait.until(lambda d : driver.find_element(By.XPATH, change_currency_link)).is_displayed()
+    change_currency_link_element = driver.find_element(By.XPATH, change_currency_link)
+    change_currency_link_element.click()
+
+def select_currency_from_dropdown(currency):
+    currency_dropdown_element = driver.find_element(By.XPATH, currency_dropdown)
+    currency_dropdown_element.click()
+    specific_currency_replaced = specific_currency.replace('CURRENCY', currency)
+    specific_currency_element = driver.find_element(By.XPATH, specific_currency_replaced)
+    specific_currency_element.click()
+    confirm_currency_button_element = driver.find_element(By.XPATH, confirm_currency_button)
+    confirm_currency_button_element.click()
+
+
+def get_current_currency():
+    wait.until(EC.none_of(EC.url_contains("customer-preferences")))
+    preceding_sibling = "/preceding-sibling::span"
+    current_currency = change_currency_link + preceding_sibling
+    actions = ActionChains(driver)
+    common.perform_hover_by_xpath(language_nav_tool, actions, driver)
+    wait.until(lambda d : driver.find_element(By.XPATH, change_currency_link)).is_displayed()
+    current_currency_text = driver.find_element(By.XPATH, current_currency).text
+    current_currency_text = re.match(".* *(\\w{3}) - .+", current_currency_text).group(1)
+    return current_currency_text
